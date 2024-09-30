a0f6610833d908082339bd9e9d07cde0d1d7c6a1
Sankalp2411 <jadhavsankalp2004>
2024-09-30 09:11:41 +0530

    add code

--- ASMD-inC.c
+++ ASMD-inC.c
@@ -3,28 +3,28 @@ int main()
 {
     int number1,number2,result,operation;
     printf("enter first number\n");
-    scanf("%d\n",&number1);
+    scanf("%d",&number1);
     printf("enter second number\n");
-    scanf("%d\n",&number2);
+    scanf("%d",&number2);
     printf("OPERATIONS\n1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n");
     printf("enter operation\n");
-    scanf("%d\n"&operation);
-    if opertion == 1
+    scanf("%d",&operation);
+    if (operation == 1)
     {
         result = number1 + number2;
         printf("addition of %d and %d is %d",number1,number2,result);
     }
-    else if operator == 2
+    else if (operation == 2)
     {
         result = number1 - number2;
         printf("substraction of %d and %d is %d",number1,number2,result);
     }
-    else if operator == 3
+    else if (operation == 3)
     {
         result = number1 * number2;
         printf("multiplication of %d and %d is %d",number1,number2,result);
     }
-    else if operator == 4
+    else if (operation == 4)
     {
         result = number1 / number2;
         printf("division of %d and %d is %d",number1,number2,result);
